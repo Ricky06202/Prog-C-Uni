e88920b797cf3dd34b6e0b266fa7a7e885141063
Ricardo Sanjur <ricardosanjurg@gmail.com>
2021-11-09 09:20:04 -0500

    Se agregaron limites maximos y se agrego documentacion

--- Programacion1/JuegoDeLaVida.c
+++ Programacion1/JuegoDeLaVida.c
@@ -1,3 +1,13 @@
+/**
+ * @archivo JuegoDeLaVida.c
+ * @autor Ricardo Sanjur (ricardosanjurg@gmail.com)
+ * @resumen
+ * @version 0.3
+ * @fecha 09-11-2021
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -5,6 +15,12 @@
 
 #define TITULO "", 3, "      Juego De La Vida", "2IL112", 2021, "2do", true, "Cecilia Gonzalez"
 
+/**
+ * @resumen Encargada de poner el borde en la matriz
+ *
+ * @parametro tam
+ * @parametro matriz
+ */
 void ponerElMarco(int tam, char matriz[tam][tam])
 {
   for (int i = 0; i < tam; i++)
@@ -51,7 +67,12 @@ void ponerElMarco(int tam, char matriz[tam][tam])
     ------------------------------------- ← fila recibida - 1
   */
 }
-
+/**
+ * @resumen Encargada de poner los asteriscos en forma de cruz en la matriz
+ *
+ * @parametro tam
+ * @parametro matriz
+ */
 void ponerLosAsteriscos(int tam, char matriz[tam][tam])
 {
   for (int i = 1, j = tam - 2; i < tam - 1; i++, j--)
@@ -73,7 +94,17 @@ void ponerLosAsteriscos(int tam, char matriz[tam][tam])
 
    */
 }
-
+/**
+ * @resumen Encargada de verificar los arrededores de una casilla de la matriz
+ * devolviendo true si encuentra 3 asteriscos contando la casilla objetivo
+ *
+ * @parametro tam
+ * @parametro matriz
+ * @parametro posicionY
+ * @parametro posicionX
+ * @return true
+ * @return false
+ */
 bool registrarAsteriscos(int tam, char matriz[tam][tam], int posicionY, int posicionX)
 {
   int contar = 0;
@@ -99,7 +130,16 @@ bool registrarAsteriscos(int tam, char matriz[tam][tam], int posicionY, int posi
   solo verificar la posicion recibida
   */
 }
-
+/**
+ * @resumen Si registrarAsterisco devuelve true va a poner un asterisco en la posicion
+ * donde dio true en la nueva dimension
+ *
+ * @parametro tam
+ * @parametro matrizActualizada
+ * @parametro posicionY
+ * @parametro posicionX
+ * @parametro registrarAsterisco
+ */
 void nuevoAsterisco(int tam, char matrizActualizada[tam][tam], int posicionY, int posicionX, bool registrarAsterisco)
 {
   if (registrarAsterisco)
@@ -110,7 +150,13 @@ void nuevoAsterisco(int tam, char matrizActualizada[tam][tam], int posicionY, in
   en la posicion recibida en la matrizActualizada(osea la otra matriz)
   */
 }
-
+/**
+ * @resumen imprime las dos matrices una alado de la otra
+ *
+ * @parametro tam
+ * @parametro matriz
+ * @parametro matrizActualizada
+ */
 void mostrarMatriz(int tam, char matriz[tam][tam], char matrizActualizada[tam][tam])
 {
   for (int f = 0; f < tam; f++)
@@ -125,14 +171,26 @@ void mostrarMatriz(int tam, char matriz[tam][tam], char matrizActualizada[tam][t
   }
   printf("\n\n");
 }
-
+/**
+ * @resumen asigna a todas las casillas de la matriz a espacio en blanco ' '
+ *
+ * @parametro tam
+ * @parametro matriz
+ */
 void limpiarMatriz(int tam, char matriz[tam][tam])
 {
   for (int f = 0; f < tam; f++)
     for (int c = 0; c < tam; c++)
       matriz[f][c] = ' ';
 }
-
+/**
+ * @resumen verifica si aun hay asteriscos en la matriz
+ *
+ * @parametro tam
+ * @parametro matriz
+ * @return true
+ * @return false
+ */
 bool sigueAlguienVivo(int tam, char matriz[tam][tam])
 {
   for (int i = 1; i < tam - 1; i++)
@@ -141,7 +199,16 @@ bool sigueAlguienVivo(int tam, char matriz[tam][tam])
         return true;
   return false;
 }
-
+/**
+ * @resumen verifica si las matrices son exactamente iguales y si aun tienen asteriscos
+ * si es asi devuelve true
+ *
+ * @parametro tam
+ * @parametro matriz
+ * @parametro matrizActualizada
+ * @return true
+ * @return false
+ */
 bool hayInmortalidad(int tam, char matriz[tam][tam], char matrizActualizada[tam][tam])
 {
   bool respuesta = false;
@@ -164,9 +231,9 @@ int main(int argc, char const *argv[])
     {
       printf("Ingrese el tama%co de la Matriz Cuadrada", 164);
       tam = weIngresarEntero("");
-      if (tam >= 5)
+      if (tam >= 5 && tam <= 50)
         break;
-      printf("**Debe-Ser-5-o-Mayor**\n");
+      printf("**Debe-Ser-Entre-5-y-50**\n");
     }
 
     char universo1[tam][tam], universo2[tam][tam], universoAux[tam][tam];
@@ -183,9 +250,9 @@ int main(int argc, char const *argv[])
     while (true) // este ciclo nos permite estar seguros de que el usuario ingreso un numero valido
     {
       limite = weIngresarEntero("Ingrese la Cantidad de Generaciones");
-      if (limite >= 1)
+      if (limite >= 1 && limite <= 1000)
         break;
-      printf("**Debe-Ser-1-o-Mayor**\n");
+      printf("**Debe-Ser-Entre-1-y-1000**\n");
     }
 
     wePresentacion(TITULO);
