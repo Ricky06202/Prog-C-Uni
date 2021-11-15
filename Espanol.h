f0d2efe2470218ac21667b5473134eeebf5436e1
Ricky06202 <91886986+Ricky06202@users.noreply.github.com>
2021-11-15 16:03:08 -0500

    Español para C
    Traduce el lenguaje C para programar en español

---
+++ Espanol.h
@@ -0,0 +1,42 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdbool.h"
+// main
+#define principal main
+#define regresa return
+
+// Tipos de Variables
+#define corto short
+#define entero int
+#define largo long
+#define flotante float
+#define doble double
+#define caracter char
+#define logico bool
+#define vacio void
+#define constante const
+
+// bucles y condiciones
+#define si if
+#define sino else
+#define hacer do
+#define mientras while
+#define para for
+#define segunSea switch
+#define caso case
+#define romper break
+#define deOtroModo default
+
+// simbolos condiciones
+#define Y &&
+#define O ||
+#define cierto true
+#define falso false
+
+// comandos
+#define imprimir printf
+#define leer scanf
+#define irA goto
+
+
+    
