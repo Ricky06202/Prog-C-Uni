25568d35b52039bcffee890e4ab4606c34f20192
Ricardo Sanjur <ricardosanjurg@gmail.com>
2021-11-17 09:16:10 -0500

    Version Inicial 0.1

---
+++ Programacion1/ProyectoIndividual.c
@@ -0,0 +1,22 @@
+/**
+ * @archivo ProyectoIndividual.c
+ * @autor Ricardo Sanjur (ricardosanjurg@gmail.com)
+ * @resumen Hay que leer de un archivo llamado "Datos.txt" la informacion
+ *          de ciertos alumnos y procesarla, los resultados de esta
+ *          se almacenaran en un archivo nuevo llamado "DatosProcesados.txt"
+ * @version 0.1
+ * @fecha 17-11-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "stdio.h"
+#include "stdlib.h"
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
