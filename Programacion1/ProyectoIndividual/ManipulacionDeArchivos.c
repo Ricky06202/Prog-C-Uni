bfa672529b12c7fb514d4f870b3f0e4cc3d2d036
Ricardo Sanjur <ricardosanjurg@gmail.com>
2021-11-17 15:19:03 -0500

    Se realizaron mejoras de memoria utilizando menos variables 0.4

--- Programacion1/ProyectoIndividual/ManipulacionDeArchivos.c
+++ Programacion1/ProyectoIndividual/ManipulacionDeArchivos.c
@@ -4,7 +4,7 @@
  * @resumen Hay que leer de un archivo llamado "DATOS.txt" la informacion
  *          de ciertos alumnos y procesarla, los resultados de esta
  *          se almacenaran en un archivo nuevo llamado "DatosProcesados.txt"
- * @version 0.3
+ * @version 0.4
  * @fecha 17-11-2021
  *
  * @copyright Copyright (c) 2021
@@ -12,6 +12,7 @@
  */
 #include "stdio.h"
 #include "stdlib.h"
+
 #define CANTIDAD_DE_SEMESTRES 3
 #define CANTIDAD_DE_MATERIAS 6
 struct alumno
@@ -19,15 +20,10 @@ struct alumno
     char nombre[25];
     int periodo[CANTIDAD_DE_SEMESTRES];
     int semestre[CANTIDAD_DE_SEMESTRES];
-    int creditos;
-    char codigoAsignatura[5];
-    char nota;
-    char descripcion[50];
 
     int creditosTotales[CANTIDAD_DE_SEMESTRES];
     int puntos[CANTIDAD_DE_SEMESTRES];
     float indice[CANTIDAD_DE_SEMESTRES];
-    int condicional;
 };
 /**
  * @resumen nos permite calcular la cantidad de puntos que se obtubo gracias a la
@@ -58,19 +54,20 @@ int main(int argc, char const *argv[])
     while (!feof(datos))
     {
         fscanf(datos, "%[^\n]\n", &estudiante[conteoAlumnos].nombre);
-        estudiante[conteoAlumnos].condicional = 0;
         for (int i = 0; i < CANTIDAD_DE_SEMESTRES; i++)
         {
             fscanf(datos, "%d %d\n", &estudiante[conteoAlumnos].periodo[i], &estudiante[conteoAlumnos].semestre[i]);
-            estudiante[conteoAlumnos].creditosTotales[i] = 0;
             estudiante[conteoAlumnos].puntos[i] = 0;
+            estudiante[conteoAlumnos].creditosTotales[i] = 0;
             for (int j = 0; j < CANTIDAD_DE_MATERIAS; j++)
             {
-                fscanf(datos, "%d %s %c %[^\n]\n", &estudiante[conteoAlumnos].creditos, &estudiante[conteoAlumnos].codigoAsignatura, &estudiante[conteoAlumnos].nota, &estudiante[conteoAlumnos].descripcion);
-                estudiante[conteoAlumnos].creditosTotales[i] += estudiante[conteoAlumnos].creditos;
-                estudiante[conteoAlumnos].puntos[i] += calcularPuntos(estudiante[conteoAlumnos].creditos, estudiante[conteoAlumnos].nota);
+                int creditos;
+                char codigoAsignatura[5], descripcion[50], nota;
+                fscanf(datos, "%d %s %c %[^\n]\n", &creditos, &codigoAsignatura, &nota, &descripcion);
+                estudiante[conteoAlumnos].creditosTotales[i] += creditos;
+                estudiante[conteoAlumnos].puntos[i] += calcularPuntos(creditos, nota);
             }
-            estudiante[conteoAlumnos].indice[i] = (float)estudiante[conteoAlumnos].puntos[i] / (float)estudiante[conteoAlumnos].creditosTotales[i];
+            estudiante[conteoAlumnos].indice[i] = (float)estudiante[conteoAlumnos].puntos[i] / estudiante[conteoAlumnos].creditosTotales[i];
         }
         conteoAlumnos++;
     }
@@ -79,20 +76,23 @@ int main(int argc, char const *argv[])
     FILE *salida = fopen("DatosProcesados.txt", "w");
     for (int i = 0; i < CANTIDAD_DE_ALUMNOS; i++)
     {
-        fprintf(salida, "%30s   Semestre   Puntos   Total/Cr   Indice   Condicional\n", estudiante[i].nombre);
-        estudiante[i].condicional = 0;
+        if (i != 0)
+            fprintf(salida, "\n");
+        fprintf(salida, "%s", estudiante[i].nombre);
+        fprintf(salida, "\n      Semestre   Puntos   Total/Cr   Indice   Condicional", estudiante[i].nombre);
+        int condicional = 0;
         for (int j = 0; j < CANTIDAD_DE_SEMESTRES; j++)
         {
             if (estudiante[i].indice[j] < 1)
-                estudiante[i].condicional += 1;
+                condicional += 1;
             else
-                estudiante[i].condicional = 0;
-            fprintf(salida, "%25d  %11d   %7d   %7d   %8.2f   %7d", estudiante[i].periodo[j], estudiante[i].semestre[j], estudiante[i].puntos[j], estudiante[i].creditosTotales[j], estudiante[i].indice[j], estudiante[i].condicional);
-            fprintf(salida, "\n");
+                condicional = 0;
+            fprintf(salida, "\n%d  %5d   %7d   %7d   %8.2f   %7d", estudiante[i].periodo[j], estudiante[i].semestre[j], estudiante[i].puntos[j], estudiante[i].creditosTotales[j], estudiante[i].indice[j], condicional);
         }
-        if(estudiante[i].condicional == 3)
-            fprintf(salida, "                    CAMBIO DE CARRERA\n");
-        fprintf(salida, "\n");
+        if (condicional == 3)
+            fprintf(salida, "       CAMBIO DE CARRERA\n");
+        if (i != CANTIDAD_DE_ALUMNOS - 1)
+            fprintf(salida, "\n");
     }
     fclose(salida);
     return 0;
