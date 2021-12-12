4613fadbd73200d786a124e4284a6ade6089c46c
Ricardo A Sanjur G <91886986+Ricky06202@users.noreply.github.com>
2021-12-12 13:36:33 -0500

    Se realizo una funcion para mostrar el inventario usando punteros de struct v0.2

--- Programacion1/ricardoSanjurSemestral/semestralRicardoSanjur.c
+++ Programacion1/ricardoSanjurSemestral/semestralRicardoSanjur.c
@@ -4,12 +4,12 @@
  * @resumen En este proyecto intentamos hacer un menu en el que podamos
  * pedir los articulos a vender, hacer un registro de todos los articulos q tenemos
  * y poder procesar las ordenes de los clientes para la venta de los productos
- * 
- * @version 0.1
- * @fecha 10-12-2021
- * 
+ *
+ * @version 0.2
+ * @fecha 12-12-2021
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 #include "stdio.h"
 #include "stdlib.h"
@@ -18,46 +18,144 @@
 /**
  * @resumen Esta Funcion se encarga de pedir los articulos y registrarlos en
  * el archivo "clientesRicardoSanjur.txt"
+ *
+ */
+typedef struct datosArticulos
+{
+    char codigo[10], descripcion[30], aux[10], impuesto;
+    int cantidad;
+    float costo;
+} datosArticulos;
+
+/**
+ * @resumen Esta Funcion se encarga de pedir los articulos y registrarlos en
+ * el archivo "clientesRicardoSanjur.txt"
  * 
  */
 void almacenarArticulos()
 {
-    char codigo[10], descripcion[50], aux[10], impuesto;
-    int cantidad;
-    float costo;
+    datosArticulos articulo;
 
-    weIngresarTexto(codigo, "Ingrese el Codigo del Articulo");
-    weIngresarTexto(descripcion, "Ingrese la Descripcion del Articulo");
+    weIngresarTexto(articulo.codigo, "Ingrese el Codigo del Articulo");
+    weIngresarTexto(articulo.descripcion, "Ingrese la Descripcion del Articulo");
     while (true)
     {
-        weIngresarTexto(aux, "Ingrese El Impuesto");
-        impuesto = aux[0];
-        if (impuesto >= '0' && impuesto <= '9')
+        weIngresarTexto(articulo.aux, "Ingrese El Impuesto");
+        articulo.impuesto = articulo.aux[0];
+        if (articulo.impuesto >= '0' && articulo.impuesto <= '9')
             break;
         printf("Error!! El impuesto debe ser un numero entre 0 y 9\n");
     }
     while (true)
     {
-        weIngresarTexto(aux, "Ingrese La Cantidad Existente");
-        cantidad = atoi(aux);
-        if (cantidad > 0)
+        weIngresarTexto(articulo.aux, "Ingrese La Cantidad Existente");
+        articulo.cantidad = atoi(articulo.aux);
+        if (articulo.cantidad > 0)
             break;
         printf("Debe ser un numero positivo\n");
     }
     while (true)
     {
-        weIngresarTexto(aux, "Ingrese el Costo Unitario");
-        costo = atof(aux);
-        if (costo > 0)
+        weIngresarTexto(articulo.aux, "Ingrese el Costo Unitario");
+        articulo.costo = atof(articulo.aux);
+        if (articulo.costo > 0)
             break;
         printf("Debe ser un numero positivo\n");
     }
 
     FILE *clientes = fopen("clientesRicardoSanjur.txt", "a");
-    fprintf(clientes, "%s - %s\n", codigo, descripcion);
-    fprintf(clientes, "'%c' - %d - %.2f\n", impuesto, cantidad, costo);
+    fprintf(clientes, "%s - %s\n", articulo.codigo, articulo.descripcion);
+    fprintf(clientes, "'%c' - %d - %.2f\n", articulo.impuesto, articulo.cantidad, articulo.costo);
     fclose(clientes);
 }
+
+/**
+ * @resumen esta funcion se encarga de contar la cantidad de lineas que posee
+ * el archivo con los articulos y regresa la cantidad de articulos
+ * 
+ * @return int 
+ */
+int obtenerCantidadDeArticulos()
+{
+    FILE *clientes = fopen("clientesRicardoSanjur.txt", "r");
+    if (clientes == NULL)
+    {
+        printf("No se encontro el Archivo \"clientesRicardoSanjur.txt\"");
+        wePausa;
+        exit(-1);
+    }
+    int cantidadArticulos = 0;
+    while (!feof(clientes))
+    {
+        char aux[50];
+        fgets(aux, 50, clientes);
+        cantidadArticulos++;
+    }
+    fclose(clientes);
+    return cantidadArticulos /= 2;
+}
+
+/**
+ * @resumen esta funcion se encarga de leer los datos de los articulos existentes y
+ * almacenarlos en un arreglo y lo regresa como un puntero para poder usarlo mas adelante
+ * 
+ * @return datosArticulos* 
+ */
+datosArticulos *leerValores()
+{
+    FILE *clientes = fopen("clientesRicardoSanjur.txt", "r");
+    if (clientes == NULL)
+    {
+        printf("No se encontro el Archivo \"clientesRicardoSanjur.txt\"");
+        wePausa;
+        exit(-1);
+    }
+
+    const int CANTIDAD_ARTICULOS = obtenerCantidadDeArticulos();
+
+    // malloc es una funcion que me permite asignar un espacio de memoria
+    // y lo que hago es q el puntero apunte a ese espacio en memoria
+    datosArticulos *articulo = malloc(CANTIDAD_ARTICULOS * sizeof(datosArticulos));
+    for (int i = 0; i < CANTIDAD_ARTICULOS; i++)
+    {
+        // Tambien pude haber usado        articulo[i].codigo    pero queria probarlo de esta manera
+        fscanf(clientes, "%s - %[^\n]\n", &(articulo + i)->codigo, &(articulo + i)->descripcion);
+        fscanf(clientes, "'%c' - %d - %f", &(articulo + i)->impuesto, &(articulo + i)->cantidad, &(articulo + i)->costo);
+    }
+    fclose(clientes);
+    return articulo;
+}
+
+#define multiplicar(a, b) a * b
+/**
+ * @resumen esta es la funcion que se encarga de registrar todos los articulos
+ * y mostrarlos haciendo un resumen del inventario y lo almacena en el
+ * archivo "salidaRicardoSanjur.txt"
+ * 
+ */
+void mostrarInventario()
+{
+    datosArticulos *articulo = leerValores();
+    float valorTotal = 0;
+    FILE *salida = fopen("salidaRicardoSanjur.txt", "a");
+
+    int CANTIDAD_ARTICULOS = obtenerCantidadDeArticulos();
+
+    fprintf(salida, "Codigo     Descripcion                       Costo    Cantidad        Valor\n");
+    for (int i = 0; i < CANTIDAD_ARTICULOS; i++)
+    {
+        // Tambien pude haber usado         articulo[i].costo    pero queria probarlo de esta manera
+        float valorcalculado = multiplicar((articulo + i)->costo, (articulo + i)->cantidad);
+
+        fprintf(salida, "%-8s   %-30s   %7.2f    %-4d     %12.2f\n", (articulo + i)->codigo, (articulo + i)->descripcion, (articulo + i)->costo, (articulo + i)->cantidad, valorcalculado);
+        valorTotal += valorcalculado;
+    }
+    fprintf(salida, "\n                                                  Valor Total   %12.2f\n", valorTotal);
+
+    fclose(salida);
+}
+
+
 
 int main(int argc, char const *argv[])
 {
